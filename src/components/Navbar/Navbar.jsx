02f116d70d6ae8b4320f58ae518e94a7696bd83e
hadis ghafouri <hadisghafouri98@gmail.com>
2022-04-10 14:07:32 +0430

    add Navbar component[mobile version]

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import "./Navbar.scss";
@@ -18,6 +20,28 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["home", "about", "work", "skills", "contact"].map(
+                (item, index) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
