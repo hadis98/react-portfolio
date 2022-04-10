77df0da7f14f528fc87405148b49839ad38d4c68
hadis ghafouri <hadisghafouri98@gmail.com>
2022-04-10 14:06:01 +0430

    add Navbar component[desktop version]

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
+
+
+import { images } from "../../constants";
 import "./Navbar.scss";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item, index) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
