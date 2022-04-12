9a64f50b07da75f075a9df2838042c8ea276dfa8
hadis ghafouri <hadisghafouri98@gmail.com>
2022-04-12 15:39:05 +0430

    change favicon,change mobile,email phots,add testimonial to menu and NavigationDots

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {["home", "about", "work", "skills", "contact"].map((item, index) => (
+        {["home", "about", "work", "skills","testimonials", "contact"].map((item, index) => (
           <li key={`link-${item}`} className="app__flex p-text">
             <div />
             <a href={`#${item}`}>{item}</a>
